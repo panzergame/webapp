3295c08e65835b157ff72840f5987d887737b60e
tristan <tristan.porteries@gmail.com>
2021-03-21 15:51:39 +0100

    Finalisation bootstrap CSS

--- pages/auth.py
+++ pages/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from forms.register_client import RegisterClientForm
 from forms.login_client import LoginClientForm
 from flask_login import login_user, login_required, logout_user
@@ -14,7 +14,8 @@ def register_client():
 		client = Client.create(form.username.data, form.email.data, form.password.data)
 		login_user(client)
 
-		return redirect(url_for('product.products'))
+		next = request.args.get('next')
+		return redirect(next)
 
 	return render_template('register_client.html', form=form)
 
@@ -26,7 +27,8 @@ def login_client():
 		client = Client.get_by_credential(form.email.data, form.password.data)
 		login_user(client, remember=form.remember.data)
 
-		return redirect(url_for('product.products'))
+		next = request.args.get('next')
+		return redirect(next)
 
 	return render_template('login_client.html', form=form)
 
